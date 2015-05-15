89a7dacb64f45d1ea3d7e0907f7eae5012ad23bb
Jeff Dillon <dillonjg@gmail.com>
2015-05-14 19:41:02 -0600

    initial index.php commit

---
+++ index.php
@@ -0,0 +1,17 @@
+/*
+ * main controller for geeklife.social
+ */
+
+
+$actions = array();
+$publicActions[0] = 'showHomePage';
+$publicActions[1] = 'showDetailPage';
+$publicActions[2] = 'showEventCreate';
+$publicActions[3] = 'doRSVP';
+
+if($action == 'showHomePage') {
+	//get the list of events
+	
+	//display the page
+	echo "Hello, World!";
+}
