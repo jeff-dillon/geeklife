7c97d7a79f0eeac1d0be62f7610b320f6e87a895
Dawson Botsford <DawsonBotsford@gmail.com>
2015-05-14 20:23:14 -0600

    Update page looks

--- includes/detailPage.php
+++ includes/detailPage.php
@@ -6,9 +6,31 @@
 <html>
 	<head><title>Event Detail</title>
 		
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></link>
+    <link rel="stylesheet" href="../style.css"></link>
 	</head>
-	<body>
-		<h1>Event Detail</h1>
-	</body>
+<body>
+  <header>
+    <img src="http://i.imgur.com/Kh8ZJS7.png" alt="logo">
+    <nav>
+      <a href="main.html">home</a>
+    </nav>
+    <h1 class="eventname">Settlers of Catan game night!</h1>
+  </header>
+  <main>
+    <p class="hostname">Host: Nevil Longbottom</p>
+    <p class="contactemail">
+      Nevil@hogwarts.edu
+    </p>
+    <p class="location">
+      123 Hogwardts Way, Hogsmead, Scotland
+    </p>
+    <p class="discription">
+      This is going to be a super fun event where we play Settlers of Catan! All welcome, even muggles!
+      <br>
+      Bring your game faces! We'll have butter beer and fire whisky!
+    </p>
+  </main>
+
+</body>
 </html>
