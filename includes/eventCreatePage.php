407d6ed0d4cec68f224daa252bef41cb6364bcb8
Dawson Botsford <DawsonBotsford@gmail.com>
2015-05-14 20:10:11 -0600

    Form complete. Style sheet started

--- includes/eventCreatePage.php
+++ includes/eventCreatePage.php
@@ -7,5 +7,12 @@
 	<head><title>Create Event</title></head>
 	<body>
 		<h1>Create Event</h1>
+    <form action="welcome.php" method="post">
+      Your Name: <input type="text" name="name"><br>
+      E-mail: <input type="text" name="email"><br>
+      Location: <input type="text" name="email"><br>
+      Description: <input type="text" name="email"><br>
+      <input type="submit">
+    </form>
 	</body>
 </html>
